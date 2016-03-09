8a55f14a2b35f883718f2b85ec6dd002cf293f01
laura <lg3295@gmail.com>
2016-03-09 08:56:07 -1000

    Finished through function LOAN

---
+++ foreclosure.js
@@ -0,0 +1,42 @@
+'use strict'
+
+var steve;
+var stevesLoan;
+var month = 0;
+var monthsUntilEvicted;
+
+function loan() {
+	var account = {
+		borrowed : 550000,
+		balance : 286000,
+		monthlyPayment : 1700,
+		defaulted : 0,
+		defaultsToForclose : 5,
+		foreclosed : false
+	};
+	function missPayment() {
+		defaulted += 1;
+		if (account.defaulted >= account.defaultsToForclose) {
+			account.forclosed = true;
+		}
+	}
+	return {
+		getBalance : function() {
+			return account.balance;
+		}
+		receivePayment : function(amount) {
+			if (amount < account.monthlyPayment) {
+				missPayment();
+				account.balance = account.balance - amount;
+			}
+		}
+		getMonthlyPayment : function() {
+			return account.monthlyPayment;
+		}
+		isForeclosed : function() {
+			return account.foreclosed;
+		};
+	}
+
+
+}
